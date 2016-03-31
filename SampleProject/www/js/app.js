c1d487804308956a300bc8d44b8481ccebab6a16
Md. Shohanur Rahaman <shohan4556@gmail.com>
2016-03-31 09:45:56 +0600

    initial commit
    **ionic version : 1.2.4 - date : 2016-01-03
    **intelxdk version : 3088 date : 2016-01-03

---
+++ SampleProject/www/js/app.js
@@ -0,0 +1,17 @@
+(function(){
+
+var app = angular.module('starter', ['ionic']);
+      
+app.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+});
+    
+}());
